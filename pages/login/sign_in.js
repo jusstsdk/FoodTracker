f6bd01324ac32f0e76a70ec857fbf86dbc7d3fce
Angelina <angelina2003t@gmail.com>
2023-04-07 17:10:27 +0300

    feat: login using data from DB

--- pages/login/sign_in.js
+++ pages/login/sign_in.js
@@ -1,6 +1,27 @@
 const users = JSON.parse(localStorage.getItem("users"))
 
-document.querySelector(".buttonWrapper button").addEventListener('click', (event) => {
+async function getUsers() {
+    const response = await fetch('http://localhost:3000/users', { method: 'GET' });
+    return await response.json();
+}
+async function fetchUsers() {
+    try {
+        return await getUsers()
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function findUser(username, password) {
+    try {
+        const users = await fetchUsers();
+        return users.find(user => (user.username === username || user.email === username) && user.password === password)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+document.querySelector(".buttonWrapper button").addEventListener('click', async (event) => {
     event.preventDefault()
 
     const username = document.getElementById("username")
@@ -16,14 +37,7 @@ document.querySelector(".buttonWrapper button").addEventListener('click', (event
         return
     }
 
-    if (!users) {
-        alert('Something went wrong')
-        return;
-    }
-
-    const result = users.find(user => (user.username === username.value || user.email === username.value) && user.password === password.value)
-    console.log(result)
-
+    const result = await findUser(username.value, password.value)
     if (result) {
         localStorage.setItem("current_user", JSON.stringify(result))
         window.location = '../profile/profile.html'
